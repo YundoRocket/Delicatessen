c68db5bb7b768ca619436c8d7db1144612f5527b
Dams2 <damien-rojo@hotmail.fr>
2020-11-10 10:03:57 +0100

    Initial Commit

---
+++ DelicatessenTests/DelicatessenTests.swift
@@ -0,0 +1,34 @@
+//
+//  DelicatessenTests.swift
+//  DelicatessenTests
+//
+//  Created by Damien Rojo on 10.11.20.
+//  Copyright © 2020 Chimere.io LTD. All rights reserved.
+//
+
+import XCTest
+@testable import Delicatessen
+
+class DelicatessenTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
